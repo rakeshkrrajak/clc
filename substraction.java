c1ff79135c7f471246e2b55e900dd291e3a7c62a
R.Kumar <rkumar@gmail.com>
2025-12-13 11:25:45 +0530

    added new feature of substraction

---
+++ substraction.java
@@ -0,0 +1,11 @@
+package clc;
+
+public class substraction {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 10;
+        int result = num1 - num2;
+        
+        System.out.println("Substraction of " + num1 + " and " + num2 + " is: " + result);
+    }   
+}
