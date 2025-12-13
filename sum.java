aa0dd3dcb9f5c0eb86be9a82c6ec2f4342493cf7
R.Kumar <rkumar@gmail.com>
2025-12-13 10:49:16 +0530

    created sum.java for 1st time to add 2 number

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int result = num1 + num2;
+        
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
