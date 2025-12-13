378ad98169ceac1652ec088b76df987ddab32e1b
R.Kumar <rkumar@gmail.com>
2025-12-13 11:32:21 +0530

    updated multiplication feature with num1 as 40

--- multiplication.java
+++ multiplication.java
@@ -2,7 +2,7 @@ package clc;
 
 public class multiplication {
     public static void main(String[] args) {
-        int num1 = 10;
+        int num1 = 40;
         int num2 = 20;
         int result = num1 * num2;
         
